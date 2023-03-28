5c44216db4205fe82c849cd096e2ebf3affa3335
Katarina Mursec <katarina.mursec@gmail.com>
2023-03-28 22:36:27 +0200

    add draw pile and discard pile view

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import Hand from './Hand.svelte';
+  import PileSidebar from './PileSidebar.svelte';
   import { Game } from '../models/game';
+    import Header from './Header.svelte';
 
   let game: Game = new Game();
+  let isDrawPileSidebarOpen: boolean = false;
+  let isDiscardPileSidebarOpen: boolean = false;
+
   game.drawHand();
 
   function endTurn() {
@@ -10,14 +15,98 @@
     game = game;
   }
 
+  function togglePileShown(pileName) {
+    switch (pileName) {
+      case "draw":
+        isDrawPileSidebarOpen = !isDrawPileSidebarOpen;
+        break;
+      case "discard":
+        isDiscardPileSidebarOpen = !isDiscardPileSidebarOpen;
+        setDiscardPileSidebarPosition();
+        break;
+    }
+  }
+
+  function setDiscardPileSidebarPosition(): void {
+    const discardPileSidebarElement = document.getElementById("discard-pile-sidebar");
+    const discardPileButton = document.getElementById("discard-pile");
+
+    if (!discardPileSidebarElement) return;
+
+    discardPileSidebarElement.style.left = discardPileButton.offsetLeft + "px";
+  }
 </script>
 
 <div style="display: flex; flex-direction: column;">
-  <!-- Player -->
-  <!-- Enemy -->
-  <!-- Decks -->
-  <Hand game={ game }/>
-  <button on:click={ endTurn }>End turn</button>
+  <div id="combat-area" style="height: calc(100vh - 240px)">
+    <!-- Player -->
+    <!-- Enemy -->
+  </div>
+  <!-- Cards -->
+  <div class="flex-container">
+    {#if isDrawPileSidebarOpen}
+      <div id="draw-pile-sidebar" class="pile-sidebar">
+        <PileSidebar pile={ game.drawPile }/>
+      </div>
+    {/if}
+    <div id="draw-pile" class="pile-wrapper">
+      <button class="pile" on:click={ () => togglePileShown('draw') }>{ game.drawPile.length }</button>
+      <div>Draw Pile</div>
+    </div>
+    <div id="hand">
+      <Hand game={ game }/>
+      <button on:click={ endTurn }>End turn</button>
+    </div>
+    <div id="discard-pile" class="pile-wrapper">
+      <button class="pile" on:click={ () => togglePileShown('discard') }>{ game.discardPile.length }</button>
+      <div>Discard Pile</div>
+    </div>
+    <!-- {#if isDiscardPileSidebarOpen} -->
+    <div id="discard-pile-sidebar" class="pile-sidebar" class:hidden={!isDiscardPileSidebarOpen}>
+      <PileSidebar pile={ game.discardPile }/>
+    </div>
+    <!-- {/if} -->
+  </div>
 </div>
 
-<style></style>
+<style>
+  .flex-container {
+    display: flex;
+    gap: 60px;
+    justify-content: space-between;
+  }
+
+  .pile-wrapper {
+    height: 240px;
+    width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    flex-direction: column;
+  }
+
+  .pile {
+    display: block;
+    width: 80px;
+    height: 120px;
+    padding: 0.5rem;
+    border: 2px solid black;
+    border-radius: 5px;
+    background-color: gray;
+    color: black;
+    font-size: 2rem;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .pile-sidebar {
+    position: fixed;
+    top: 50px;
+    z-index: 1;
+    height: calc(100vh - 50px - 200px);
+    width: 120px;
+    border: 2px solid black;
+    border-radius: 3px;
+    overflow: hidden;
+  }
+</style>
---
+++ src/lib/PileSidebar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Card from "./Card.svelte";
+  import type { Card as CardType } from "../models/card";
+
+  export let pile: CardType[];
+</script>
+
+<div class="pile-sidebar-wrapper">
+  {#each pile as card}
+    <Card card={ card } />
+  {/each}
+</div>
+
+<style>
+  .pile-sidebar-wrapper {
+    background-color: rgba(0, 0, 0, 0.3);
+    width: 100px;
+    height: 100%;
+    padding: 10px;
+    padding-right: 60px;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    position: absolute;
+    z-index: 2;
+    left: 0;
+    overflow: auto;
+  }
+
+  .pile-sidebar-wrapper :global(.card) {
+    width: 80px;
+    height: 120px;
+    font-size: 0.8rem;
+    flex: 0 0 auto;
+    position: relative;
+    z-index: 3;
+    opacity: 0.7;
+	}
+
+  .pile-sidebar-wrapper :global(.card-title) {
+    font-size: 1rem;
+	}
+</style>
